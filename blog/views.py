e1926b7449306b018fea5930dc611502f461f8cb
wrchipman <William.Chipman@centurylink.com>
2020-03-06 14:03:57 -0700

    email coded

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render, get_object_or_404 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post 
+from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail 
+
+
+def post_share(request, post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        #form was submitted
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            #form fields passed validation
+            cd=form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at  {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    
 
 
 def post_list(request):
