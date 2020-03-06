ba0a3915349497d4dc911813af3b84b7ccd8720e
wrchipman <William.Chipman@centurylink.com>
2020-03-06 09:06:43 -0700

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
+from . import views 
+
 
 app_name = 'blog'
-
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-          views.post_detail,
-          name='post_detail'),
-]
+         views.post_detail,
+         name='post_detail'),
+] 
